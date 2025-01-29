7e4fa1a68b017a2fc87c9f6a542ebe59e3b111a9
Daniele410 <miraglia.daniele410@gmail.com>
2025-01-29 12:53:14 +0100

    add model of card

---
+++ src/main/java/com/danozzo/model/Card.java
@@ -0,0 +1,29 @@
+package com.danozzo.model;
+
+public class Card implements Comparable<Card> {
+    private final Rank rank;
+    private final Suit suit;
+
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+
+    @Override
+    public int compareTo(Card other) {
+        return this.rank.ordinal() - other.rank.ordinal();
+    }
+}
---
+++ src/main/java/com/danozzo/model/Suit.java
@@ -0,0 +1,5 @@
+package com.danozzo.model;
+
+public enum Suit {
+    CLUBS,DIAMONDS,HEARTS,SPADES;
+}
